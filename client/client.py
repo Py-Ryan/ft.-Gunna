c77e19b30196aa6bbafab9961d986c6b1db1ec7a
Py-Ryan <63752837+Py-Ryan@users.noreply.github.com>
2020-05-17 17:55:48 -0400

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -10,7 +10,6 @@ from typing import Any, Dict, List, Union, Optional
 
 
 class Ftg(commands.Bot):
-
     def __init__(self, **options: Dict[str, Any]) -> None:
         super().__init__(command_prefix=self.get_prefix_, **options)
         self.app_info: Optional[discord.AppInfo] = None
@@ -19,9 +18,12 @@ class Ftg(commands.Bot):
         self.__url__: List[str] = list()
 
         with open("client/secret/secret.json") as secret:
-            json.load(secret, object_hook=lambda d_: self.__url__.extend(
-                [d_[key] for key in d_.keys() if key == 'url']
-            ))
+            json.load(
+                secret,
+                object_hook=lambda d_: self.__url__.extend(
+                    [d_[key] for key in d_.keys() if key == "url"]
+                ),
+            )
 
         self.db = asyncio.get_event_loop().run_until_complete(
             asyncpg.create_pool(self.__url__[0], min_size=1, max_size=5)
@@ -41,23 +43,38 @@ class Ftg(commands.Bot):
         elif not isinstance(extensions, list) or not extensions:
             class_name: str = self.__class__.__name__
             func_name: str = extract_stack(None, 2)[1][2]
-            raise RuntimeWarning(f"No extensions were passed to {class_name}.{func_name}()")
+            raise RuntimeWarning(
+                f"No extensions were passed to {class_name}.{func_name}()"
+            )
 
         super().run(token, **options)
 
-    async def get_prefix_(self, bot: commands.Bot, message: discord.Message) -> Union[str, List[str]]:
-        prefix: Optional[asyncpg.Record, str] = \
-            await self.db.fetchrow("SELECT (prefix) FROM guilds WHERE id=$1", message.guild.id)
-        if message.guild.id not in self.prefix_cache or self.prefix_cache[message.guild.id] != prefix:
-            try:
-                prefix = prefix["prefix"]
-            except TypeError:
-                prefix = "gn "
-                await self.db.execute("INSERT INTO guilds (id, prefix) VALUES($1, $2)", message.guild.id, prefix)
-            finally:
-                self.prefix_cache[message.guild.id] = prefix
+    async def get_prefix_(
+        self, bot: commands.Bot, message: discord.Message
+    ) -> Union[str, List[str]]:
+        prefix: Optional[asyncpg.Record, Dict[str]] = await self.db.fetchrow(
+            """SELECT (prefix)
+                                      FROM guilds 
+                                      WHERE id=$1""",
+            message.guild.id,
+        ) or {"prefix": "gn "}
 
-        return commands.when_mentioned_or(self.prefix_cache[message.guild.id])(bot, message)
+        if (
+            message.guild.id not in self.prefix_cache
+            or self.prefix_cache[message.guild.id] != prefix["prefix"]
+        ):
+            await self.db.execute(
+                """INSERT INTO guilds (id, prefix) 
+                                     VALUES($1, $2)""",
+                message.guild.id,
+                prefix["prefix"],
+            )
+
+            self.prefix_cache[message.guild.id] = prefix["prefix"]
+
+        return commands.when_mentioned_or(self.prefix_cache[message.guild.id])(
+            bot, message
+        )
 
     async def close(self) -> None:
         await self.db.close()
@@ -74,7 +91,11 @@ class Ftg(commands.Bot):
             ctx: Context = await self.get_context(message, cls=Context)
             await self.invoke(ctx)
 
-    async def on_command_error(self, context: Context, exception: commands.CommandError) -> None:
-        if not isinstance(exception, (commands.CommandNotFound, commands.CommandOnCooldown)):
+    async def on_command_error(
+        self, context: Context, exception: commands.CommandError
+    ) -> None:
+        if not isinstance(
+            exception, (commands.CommandNotFound, commands.CommandOnCooldown)
+        ):
             await context.send(desc=str(exception))
             await context.message.add_reaction("\U0000274c")
