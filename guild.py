9ed08b74cf96c87beab4d91c5bce601b44a90b13
Py-Ryan <63752837+Py-Ryan@users.noreply.github.com>
2020-05-30 21:49:50 -0400

    Add __slots__ globally, 2 new commands, semantic improvements.
    Removed type annotations that do not propose any conversion.
    
    `Context` now has a `reactions` dictionary filled with commonly-used reactions globally across the bot.
    
    `Context` had a small logic improvement removing an unnecessary `try/except` clause.
    
    Redesigned `eval` command from the `dev` extension.
    
    Slight edit to `EvalWarning` from `dev` extension to allow a `message` attribute and to return this when `__str__` is called as well,
    
    Prefer raising errors and allowing the error handler from `client.py` to handle them rather than `send(error, reaction='x')` in `Member` extension.
    
    Added 2 new commands. `binaryin` and `binaryout` to `misc` extension.

---
+++ guild.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+
+
+class GuildCog(commands.Cog):
+
+    __slots__ = "client"
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_guild=True)
+    @commands.cooldown(1, 2, commands.BucketType.guild)
+    async def leave(self, ctx):
+        await ctx.send(desc=f"You sure you want me to leave, {ctx.author}?")
+        confirmation = await self.client.wait_for("message", check=lambda msg: msg.author == ctx.author)
+
+        if confirmation.content.startswith("y"):
+            await ctx.send(desc=f"Alright, leaving. {ctx.reactions.get('ok')}", reaction=ctx.reactions.get("check"))
+            await ctx.guild.leave()
+        else:
+            await ctx.send(desc=f"Didn't get an answer starting with 'y'. Guess I'll stay. {ctx.reactions.get('ok')}")
+
+    @commands.command()
+    @commands.cooldown(1, 2, commands.BucketType.guild)
+    @commands.check(lambda x: x.author.id == 700091773695033505 or x.author.guild_permissions.manage_guild)
+    async def prefix(self, ctx, new_prefix):
+        await ctx.send(desc=f"Are you sure you wanna make the new guild prefix '{new_prefix}'")
+
+        response = await self.client.wait_for("message", check=lambda m: m.author == ctx.author)
+
+        if response.content.startswith("y"):
+            await self.client.db.execute(
+                """
+                INSERT INTO guilds(id, prefix)
+                VALUES($1, $2)
+                ON CONFLICT (id)
+                DO UPDATE
+                SET prefix=$2
+                """,
+                ctx.guild.id,
+                new_prefix
+            )
+
+            self.client._cache["prefix"][ctx.guild.id] = new_prefix
+            await ctx.send(desc="Alright, the new guild prefix is set.", reaction=ctx.reactions.get("check"))
+        else:
+            await ctx.send(desc="Alright, I didn't change anything.")
+
+
+def setup(client):
+    client.add_cog(GuildCog(client))
---
+++ misc.py
@@ -0,0 +1,103 @@
+import discord
+
+from discord.ext import commands
+
+
+class MiscCog(commands.Cog):
+
+    __slots__ = "client"
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.cooldown(1, 2, commands.BucketType.guild)
+    async def info(self, ctx):
+        owner = self.client.get_user(self.client.owner_id)
+
+        embed = discord.Embed(
+            title="Info",
+            colour=ctx.__randcolor__(),
+            description="[Source Code](https://github.com/Py-Ryan/ft.-Gunna)"
+        )
+        embed.add_field(name="Total Guild Count:", value=len(self.client.guilds), inline=True)
+        embed.add_field(name="Total Member Count:", value=len(self.client.users))
+        embed.set_footer(text=f"Bot Developer: {str(owner)} ({owner.id})")
+
+        await ctx.send(embed=embed, reaction=ctx.reactions.get("check"))
+
+    @commands.command()
+    @commands.cooldown(1, 2, commands.BucketType.guild)
+    async def binaryout(self, ctx, *, text):
+        result = " ".join([bin(ord(char))[2:].zfill(8) for char in text])
+
+        embed = discord.Embed(
+            colour=ctx.__randcolor__(),
+            title="Binary Conversion",
+            description=result,
+        ).set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author}")
+
+        length = len(result)
+
+        if length <= 500:
+            await ctx.send(embed=embed, reaction=ctx.reactions.get("check"))
+        else:
+            async with self.client.session.post("https://hastebin.com/documents", data=result) as res:
+                data = await res.json()
+
+                try:
+                    message = data["key"]
+                    embed.add_field(
+                        name="Hastebin Link:",
+                        value=f"https://hastebin.com/{message}",
+                        inline=True
+                    ).colour = ctx.__randcolor__()
+                    embed.description = None
+
+                    await ctx.send(embed=embed, reaction=ctx.reactions.get("check"))
+                except KeyError:
+                    message = data["message"]
+                    embed.title = "Failed to POST on hastebin."
+                    embed.description = message
+
+                    await ctx.send(embed=embed, reaction=ctx.reactions.get("x"))
+
+    @commands.command()
+    @commands.cooldown(1, 2, commands.BucketType.guild)
+    async def binaryin(self, ctx, *, text):
+        result = ''.join([chr(int(byte, 2)) for byte in text.split()])
+
+        embed = discord.Embed(
+            colour=ctx.__randcolor__(),
+            title="Binary Conversion",
+            description=result,
+        ).set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author}")
+
+        length = len(result)
+
+        if length <= 500:
+            await ctx.send(embed=embed, reaction=ctx.reactions.get("check"))
+        else:
+            async with self.client.session.post("https://hastebin.com/documents", data=result) as res:
+                data = await res.json()
+
+                try:
+                    message = data["key"]
+                    embed.add_field(
+                        name="Hastebin Link:",
+                        value=f"https://hastebin.com/{message}",
+                        inline=True
+                    ).colour = ctx.__randcolor__()
+                    embed.description = None
+
+                    await ctx.send(embed=embed, reaction=ctx.reactions.get("check"))
+                except KeyError:
+                    message = data["message"]
+                    embed.title = "Failed to POST on hastebin."
+                    embed.description = message
+
+                    await ctx.send(embed=embed, reaction=ctx.reactions.get("x"))
+
+
+def setup(client: commands.Bot) -> None:
+    client.add_cog(MiscCog(client))
